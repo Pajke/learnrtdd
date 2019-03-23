f621233e7f29509b3a0b9320c7f64f525f2ce3c5
Dejan Pajic <popucanac@gmail.com>
2019-03-23 19:34:56 +0100

    hello-world

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,15 +3,31 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello returns Hello world!
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishPrefix + name
+	return languagePrefix(language) + name
+}
+
+func languagePrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
